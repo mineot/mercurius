107d3ba7786af429ef19b6472fa3b4919e3e1e6c
Mineot <mineot@gmail.com>
2024-04-28 19:55:39 -0300

    alpha/initconf - add bootstrap lib

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import axios from "axios";
+import "bootstrap";
+
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+var html = document.getElementsByTagName("html")[0];
+html.setAttribute("data-bs-theme", "dark");
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],

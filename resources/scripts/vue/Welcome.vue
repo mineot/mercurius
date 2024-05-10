403db422413b661bd974b4e5c7e6c7aaa54b5160
Mineot <mineot@gmail.com>
2024-05-09 23:45:33 -0300

    alpha/vue - layout definition

--- resources/scripts/vue/Welcome.vue
+++ resources/scripts/vue/Welcome.vue
@@ -1,24 +1,18 @@
 <template>
-  <div class="container">
-    <h1>{{ $t("comming_soon") }}</h1>
-    <h6>
-      <div>{{ $t("comming_soon_message.A") }}</div>
-      <div>{{ $t("comming_soon_message.B") }}</div>
-      <div>{{ $t("comming_soon_message.C") }}</div>
-      <div>{{ $t("comming_soon_message.D") }}</div>
-      <div>{{ $t("comming_soon_message.E") }}</div>
-    </h6>
+  <div class="layout">
+    <nav class="topbar">Top bar</nav>
+    <nav class="sidebar container-fluid">Sidebar</nav>
+    <div class="content container">
+      <div class="container">
+        <h1>{{ $t("comming_soon") }}</h1>
+        <h6>
+          <div>{{ $t("comming_soon_message.A") }}</div>
+          <div>{{ $t("comming_soon_message.B") }}</div>
+          <div>{{ $t("comming_soon_message.C") }}</div>
+          <div>{{ $t("comming_soon_message.D") }}</div>
+          <div>{{ $t("comming_soon_message.E") }}</div>
+        </h6>
+      </div>
+    </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-h1 {
-  margin-bottom: 30px;
-}
-
-h6 {
-  div {
-    margin-bottom: 30px;
-  }
-}
-</style>

5b5ec633f86fdf6caa60b3f1470cf9c5850dfcfc
mineot <mineot@gmail.com>
2024-06-27 00:32:55 -0300

    master - improvemente vite fixme

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
-// FIXME fix this problem
+// FIXME fix this problem, need get data from env by realtime
 const env = loadEnv(process.env.MODE, process.cwd());
 
 export default defineConfig({

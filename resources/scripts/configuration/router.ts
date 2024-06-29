6d34c02132d363ed59fa164d6943f3d64a9b764c
mineot <mineot@gmail.com>
2024-06-28 21:45:09 -0300

    alpha/auth - upgrade packages and protect admin

--- resources/scripts/configuration/router.ts
+++ resources/scripts/configuration/router.ts
@@ -1,9 +1,11 @@
-import registerRouters from "@helper/register-routers";
 import { createRouter, createWebHistory } from "vue-router";
+import { registerRouters, routerGuard } from "@helper/register-routers";
 
 const routerInstance = createRouter({
   history: createWebHistory(),
   routes: registerRouters,
 });
+
+routerInstance.beforeEach(routerGuard);
 
 export { routerInstance };
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PublicProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('signed', [AuthController::class, 'signed']);
 
 Route::prefix('public')->group(function () {
 

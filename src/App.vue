ac61dbf3be1d96d2a62dae695e35dea59dbdef25
mineot <mineot@gmail.com>
2024-10-05 23:12:18 -0300

    mycss - improvement layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="layout">
     <div class="header">
-      <h2 class="title">Header</h2>
-      <div class="separator"></div>
-      <div class="items">
-        <router-link class="item" to="/">
-          <i class="icon-home hover"></i>
-        </router-link>
-        <router-link class="item" to="/about">
-          <i class="icon-help hover"></i>
-        </router-link>
-      </div>
+      <router-link to="/">
+        <i class="icon-home"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/about">
+        <i class="icon-help"></i>
+        <span>About</span>
+      </router-link>
+    </div>
+    <div class="sidebar">
+      <router-link to="/">
+        <i class="icon-home"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/about">
+        <i class="icon-help"></i>
+        <span>About</span>
+      </router-link>
     </div>
     <div class="body">
       <router-view />

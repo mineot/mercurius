dcc7763ca5583602a9b26fbc9f4d3914a0822bcb
mineot <mineot@gmail.com>
2024-10-12 22:26:49 -0300

    backboostrap - new layout

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
   <div class="layout">
     <div class="header">
-      <div class="title">Title</div>
-      <div class="separator">Separator</div>
-      <div class="icons">Icons</div>
-    </div>
-
-    <div class="floatbar">
-      <div>Floatbar Item 1</div>
-      <div>Floatbar Item 2</div>
-      <div>Floatbar Item 3</div>
-    </div>
-
-    <div class="sidebar">
-      <div>Sidebar Item 1</div>
-      <div>Sidebar Item 2</div>
-      <div>Sidebar Item 3</div>
-    </div>
-
-    <div class="body">
-      <div>Body Item 1</div>
-      <div>Body Item 2</div>
-      <div>Body Item 3</div>
-      <div>Body Item 4</div>
-      <div>Body Item 5</div>
-      <div>Body Item 6</div>
-      <div>Body Item 7</div>
-      <div>Body Item 8</div>
-      <div>Body Item 9</div>
-      <div>Body Item 10</div>
-    </div>
-  </div>
-  <!-- <div class="layout">
-    <div class="header">
       <router-link to="/">
         <i class="icon-home"></i>
         <span>Home</span>
@@ -41,23 +9,10 @@
         <i class="icon-help"></i>
         <span>About</span>
       </router-link>
-    </div>
-    <div class="sidebar">
-      <div class="banner-widget">
-        <div>You Name</div>
-        <div>Figure</div>
-      </div>
-      <router-link to="/">
-        <i class="icon-home"></i>
-        <span>Home</span>
-      </router-link>
-      <router-link to="/about">
-        <i class="icon-help"></i>
-        <span>About</span>
-      </router-link>
+      <div class="separator"></div>
     </div>
     <div class="body">
       <router-view />
     </div>
-  </div> -->
+  </div>
 </template>

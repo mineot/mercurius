bc3f4ff30cb432b4c32cd3bb79609824d0650985
mineot <mineot@gmail.com>
2024-10-12 22:48:42 -0300

    mycss - some improvements

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="layout">
     <div class="header">
-      <router-link to="/">
+      <router-link class="button" to="/">
         <i class="icon-home"></i>
         <span>Home</span>
       </router-link>
-      <router-link to="/about">
+      <router-link class="button" to="/about">
         <i class="icon-help"></i>
         <span>About</span>
       </router-link>

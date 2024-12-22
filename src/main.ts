00618052434d60fea66fbc84e37f890e667da4eb
mineot <mineot@gmail.com>
2024-12-22 12:30:37 -0300

    usevite - dotenv config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import "./style.scss";
 
-console.log("Welcome to Mercurius");
+console.log(`Welcome to ${process.env.APP_NAME}`);
+
+document.title = process.env.APP_NAME ?? "Mercurius";
+
+const main = document.querySelector("main[id]");
+const h1 = document.createElement("h1");
+h1.innerText = process.env.APP_NAME ?? "Mercurius";
+main?.append(h1);

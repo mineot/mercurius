d272a40a738ef690f8a1c303dddb0e105e3412ce
mineot <mineot@gmail.com>
2024-12-30 22:03:43 -0300

    appsrc - new layout

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import "@/style.scss";
-import { Application, CurrentPage } from "@/scripts/application";
-
-import home from "@/scripts/pages/home";
+// import { Application, CurrentPage } from "@/scripts/application";
 
 document.body.onload = () => {
   document.title = process.env.APP_NAME ?? "Mercurius";
-  Application.initGlobal();
-  Application.instance.load().then((page: CurrentPage) => {
-    if (page === "home") {
-      home();
-    }
-  });
+  // Application.initGlobal();
+  // Application.instance.load().then((page: CurrentPage) => {
+  //   if (page === "home") {
+  //     // home();
+  //   }
+  // });
 };

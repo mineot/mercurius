caf2601d77937834abfecf9526acf572f19bf751
mineot <mineot@gmail.com>
2025-01-05 00:35:59 -0300

    appsrc - create header script

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import { initI18Next } from "@/scripts/i18n";
 import { setDocumentTitle } from "@/scripts/set-document-title";
 import { setFooter } from "@/scripts/set-footer";
 import { svgLoader } from "@/scripts/svg-loader";
+import { header } from "@/scripts/components/header";
 
 document.body.onload = () =>
   initI18Next().then(async (i18next) => {
     await setDocumentTitle(process);
     await setFooter(i18next, process);
     await svgLoader();
+
+    header({
+      logoUrl: "https://placehold.co/100",
+      title: "Title",
+      subtitle: "Subtitle",
+      summary:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, vitae maxime itaque nihil eveniet est aperiam dignissimos! Nam sunt, aliquam ut quis voluptas consectetur. Id nihil in molestias magnam obcaecati!",
+    });
+
     // await App.initGlobal();
     // await App.instance.load(i18next);
   });
---
+++ src/scripts/components/header.ts
@@ -0,0 +1,37 @@
+export interface HeaderParameters {
+  logoUrl: string;
+  title: string;
+  subtitle: string;
+  summary: string;
+}
+
+export async function header(args: HeaderParameters) {
+  const img: HTMLElement = document.createElement("img");
+  img.classList.add("border-primary");
+  img.setAttribute("src", args.logoUrl);
+  img.setAttribute("alt", "App Main Image");
+
+  const figure: HTMLElement = document.createElement("figure");
+  figure.append(img);
+
+  const h1: HTMLElement = document.createElement("h1");
+  h1.classList.add("text-primary");
+  h1.innerHTML = args.title;
+
+  const span: HTMLElement = document.createElement("span");
+  span.classList.add("text-secondary");
+  span.innerHTML = args.subtitle;
+
+  const p: HTMLElement = document.createElement("p");
+  p.classList.add("text-small");
+  p.innerHTML = args.summary;
+
+  const section: HTMLElement = document.createElement("section");
+  section.append(h1);
+  section.append(span);
+  section.append(p);
+
+  const header = document.body.querySelector("header");
+  header?.append(figure);
+  header?.append(section);
+}

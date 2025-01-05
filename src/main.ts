4878905da14cef8e289827bd20c1d77b69ba7d2c
mineot <mineot@gmail.com>
2025-01-04 23:31:37 -0300

    appsrc - reconfigure application script

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
 import "@/main.scss";
+import { App } from "@/scripts/application";
 import { initI18Next } from "@/scripts/i18n";
 import { setCopyright } from "@/scripts/set-copyright";
 import { setDocumentTitle } from "@/scripts/set-document-title";
 import { setFooter } from "@/scripts/set-footer";
 import { svgLoader } from "@/scripts/svg-loader";
-// import { Application, CurrentPage } from "@/scripts/application";
 
-document.body.onload = () => {
-  initI18Next().then((i18next) => {
+document.body.onload = () =>
+  initI18Next().then(async (i18next) => {
     svgLoader();
     setCopyright(process);
     setDocumentTitle(process);
     setFooter(i18next);
 
-    // Application.initGlobal();
-    // Application.instance.load().then((page: CurrentPage) => {
-    //   if (page === "home") {
-    //     // home();
-    //   }
-    // });
+    await App.initGlobal();
+    await App.instance.load(i18next);
   });
-};
--- src/scripts/application.ts
+++ src/scripts/application.ts
@@ -1,73 +1,56 @@
-export type CurrentPage = "home" | null;
+import { i18n } from "i18next";
+
+type PageName = "home" | null;
 
 export interface Param {
   name: string;
   value: string;
 }
 
-export class Application {
-  private $currentPage: CurrentPage = null;
+interface Href {
+  page?: PageName;
+  params?: Param[];
+}
+
+export class App {
+  private $page: PageName = null;
 
   static async initGlobal() {
-    (window as any).$app = new Application();
+    (window as any).$app = new App();
   }
 
-  static get instance(): Application {
+  static get instance(): App {
     return (window as any).$app;
   }
 
-  setPage(page: CurrentPage, params?: Param[]): void {
-    let href = "/";
-
-    if (page !== "home" && page !== null) {
-      const qs = new URLSearchParams();
-
-      qs.append("page", page ?? "home");
-
-      if (params) {
-        for (let p of params) {
-          qs.append(p.name, p.value);
-        }
-      }
-
-      href = `/?${qs.toString()}`;
+  href({ page, params }: Href = {}): void {
+    if (!page || page === "home") {
+      this.$page === "home";
+      location.href = "/";
+      return;
     }
 
-    this.$currentPage = page ?? "home";
-    location.href = href;
+    const qs = new URLSearchParams();
+    qs.append("page", page ?? "home");
+
+    if (params) {
+      for (let { name, value } of params) {
+        qs.append(name, value);
+      }
+    }
+
+    location.href = `/?${qs.toString()}`;
   }
 
-  getParam(name: string): Param | null {
+  get(key: string): Param | null {
     const qs = new URLSearchParams(location.search);
-    const value = qs.get(name);
-    return value ? { name, value } : null;
+    const value = qs.get(key);
+    return value ? { name: key, value } : null;
   }
 
-  async load(): Promise<CurrentPage> {
-    try {
-      const qs = new URLSearchParams(location.search);
-      const page: CurrentPage = (qs.get("page") as CurrentPage) ?? "home";
-
-      if (page !== this.$currentPage) {
-        const response = await fetch(`./pages/${page}.html`);
-
-        if (!response.ok) {
-          throw { status: response.status, message: response.statusText };
-        }
-
-        const main = document.getElementById("app");
-
-        if (main) {
-          main.innerHTML = await response.text();
-        } else {
-          throw { status: 404, message: "Main element not found!" };
-        }
-      }
-
-      return page;
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
+  async load(i18next: i18n): Promise<void> {
+    const qs = new URLSearchParams(location.search);
+    const page = qs.get("page") ?? "home";
+    console.log({ page, i18next });
   }
 }

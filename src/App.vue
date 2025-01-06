df9c8cbc26312897c0efb2a3395be388163d0723
mineot <mineot@gmail.com>
2025-01-06 00:25:27 -0300

    usevue - create routers

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <div>
+      <RouterLink to="/">Home</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/resume">Resume</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/portfolio">Portfolio</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/products">Products</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/services">Services</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/articles">Articles</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/blog">Blog</RouterLink>
+    </div>
+    <div>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
   </nav>
 
   <RouterView />
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import AboutView from "@/views/AboutView.vue";
+import ArticlesView from "@/views/ArticlesView.vue";
+import BlogView from "@/views/BlogView.vue";
+import HomeView from "@/views/HomeView.vue";
+import PortfolioView from "@/views/PortfolioView.vue";
+import ProductsView from "@/views/ProductsView.vue";
+import ResumeView from "@/views/ResumeView.vue";
+import ServicesView from "@/views/ServicesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/resume",
+      name: "resume",
+      component: ResumeView,
+    },
+    {
+      path: "/portfolio",
+      name: "portfolio",
+      component: PortfolioView,
+    },
+    {
+      path: "/products",
+      name: "products",
+      component: ProductsView,
+    },
+    {
+      path: "/services",
+      name: "services",
+      component: ServicesView,
+    },
+    {
+      path: "/articles",
+      name: "articles",
+      component: ArticlesView,
+    },
+    {
+      path: "/blog",
+      name: "blog",
+      component: BlogView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
     },
   ],
-})
+});
 
-export default router
+export default router;

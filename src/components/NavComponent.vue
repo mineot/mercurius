569bcaad3f9bb5987030e57e9ee95801263e1c23
mineot <mineot@gmail.com>
2025-01-07 22:04:13 -0300

    usevue - separate layouts

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <RouterLink to="/">
+      <app-icon type="home" />
+      <span>Home</span>
+    </RouterLink>
+    <RouterLink to="/resume">
+      <app-icon type="resume" />
+      <span>Resume</span>
+    </RouterLink>
+    <RouterLink to="/portfolio">
+      <app-icon type="portfolio" />
+      <span>Portfolio</span>
+    </RouterLink>
+    <RouterLink to="/products">
+      <app-icon type="product" />
+      <span>Products</span>
+    </RouterLink>
+    <RouterLink to="/services">
+      <app-icon type="service" />
+      <span>Services</span>
+    </RouterLink>
+    <RouterLink to="/articles">
+      <app-icon type="article" />
+      <span>Articles</span>
+    </RouterLink>
+    <RouterLink to="/blog">
+      <app-icon type="blog" />
+      <span>Blog</span>
+    </RouterLink>
+    <RouterLink to="/contact">
+      <app-icon type="contact" />
+      <span>Contact</span>
+    </RouterLink>
+    <RouterLink to="/about">
+      <app-icon type="about" />
+      <span>About</span>
+    </RouterLink>
+  </nav>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 
+import FooterComponent from "@/components/FooterComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
 import IconComponent from "@/components/IconComponent.vue";
 import LayoutComponent from "@/components/LayoutComponent.vue";
+import NavComponent from "@/components/NavComponent.vue";
 
 const app = createApp(App);
-app.component("app-layout", LayoutComponent);
+app.component("app-footer", FooterComponent);
+app.component("app-header", HeaderComponent);
 app.component("app-icon", IconComponent);
+app.component("app-layout", LayoutComponent);
+app.component("app-nav", NavComponent);
 app.use(router);
 app.mount("#app");

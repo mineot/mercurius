fad7cf7689ef5aac797ee35f22fb56f19cf4a0e5
mineot <mineot@gmail.com>
2025-01-12 16:47:06 -0300

    usevue - build header

--- src/components/widgets/HeaderWidget.vue
+++ src/components/widgets/HeaderWidget.vue
@@ -1,6 +1,19 @@
 <template>
   <header>
-    <span>Header</span>
+    <section>
+      <figure>
+        <img src="https://placehold.co/120" alt="Header Image" />
+      </figure>
+      <section>
+        <h1 class="text-primary">Title</h1>
+        <div class="text-secondary text-large">Subtitle</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quia magnam sapiente sit id quibusdam
+          autem deserunt, rem itaque ab inventore, aperiam enim, odio debitis? Sequi obcaecati suscipit laborum
+          adipisci.
+        </div>
+      </section>
+    </section>
     <NavWidget />
   </header>
 </template>

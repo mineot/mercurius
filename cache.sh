cdb35d75fd7c96bb8b26f58a10cc827cc50a395f
mineot <mineot@gmail.com>
2025-01-22 21:12:29 -0300

    uselaravel - create a new project

--- cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./cache_clear.sh
-php artisan config:cache
-php artisan view:cache
-php artisan route:cache
-php artisan event:cache
-
--- cache_clear.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-php artisan cache:clear
-php artisan cache:forget key
-php artisan route:clear
-php artisan config:clear
-php artisan view:clear
-php artisan optimize:clear

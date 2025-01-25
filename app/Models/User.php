419f501eef516d1a02523cc0ce9803fada855d8f
mineot <mineot@gmail.com>
2025-01-25 16:47:21 -0300

    uselaravel - fix seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,7 +12,10 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasUuids, Notifiable;
+
+    /** Treat the primary key as a string. */
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $user = new User();
+        $user->name = 'Administrator';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('Admin@123456');
+        $user->save();
     }
 }

329547c564c6b2843cb4a77ccb3bb9c4e29060b1
mineot <mineot@gmail.com>
2025-01-27 22:01:26 -0300

    uselaravel - remove bootstrap

--- resources/scripts/app.ts
+++ resources/scripts/app.ts
@@ -1,7 +1,11 @@
-import "./bootstrap";
 import { createApp } from "vue";
+import axios from "axios";
 
 import Welcome from "vues/Welcome.vue";
+
+(window as any).axios = axios;
+(window as any).axios.defaults.headers.common["X-Requested-With"] =
+    "XMLHttpRequest";
 
 const app = createApp({});
 app.component("v-welcome", Welcome);
--- resources/scripts/bootstrap.ts
+++
@@ -1,6 +0,0 @@
-import "node_modules/bootstrap/dist/js/bootstrap.min";
-
-import axios from "axios";
-(window as any).axios = axios;
-(window as any).axios.defaults.headers.common["X-Requested-With"] =
-    "XMLHttpRequest";

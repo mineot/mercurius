54db8effbfb6d0dacd21ccdf959283dcf80a116c
mineot <mineot@gmail.com>
2025-01-29 20:24:10 -0300

    uselaravel -

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,20 @@
-<x-guest>
-    <h1>Mercurius</h1>
-</x-guest>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name') }}</title>
+        @vite(['resources/sass/app.scss', 'resources/scripts/app.ts'])
+    </head>                         
+    <body>
+        <section id="app">
+            <header>Header</header>
+            <nav>Nav</nav>
+            <main>
+                <i class="bi bi-house"></i>
+                Main
+            </main>
+            <footer>Footer</footer>
+        </section>
+    </body>
+</html>

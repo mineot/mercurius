615ec51975584b47a720780f207c9396a379dcaa
mineot <mineot@gmail.com>
2025-01-30 22:49:35 -0300

    master - define a new language

--- config/app.php
+++ config/app.php
@@ -84,6 +84,8 @@ return [
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
+    'supported_locales' => ['en', 'pt'],
+
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
---
+++ lang/pt/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Sua senha foi redefinida.',
+    'sent' => 'Enviamos um e-mail com o link para redefinição de senha.',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Este token de redefinição de senha é inválido.',
+    'user' => 'Não conseguimos encontrar um usuário com esse endereço de e-mail.',
+];

55046825168b21a4582d303f7fbaeb5bebb92b4b
mineot <mineot@gmail.com>
2025-02-02 19:55:38 -0300

    master - create background, text, border and hr

--- resources/components/LayoutComponent.vue
+++ resources/components/LayoutComponent.vue
@@ -1,6 +1,54 @@
 <template>
   <header>Header</header>
   <nav>Navigation</nav>
-  <main>Main</main>
+  <main>
+    <br />
+    <div ref="container"></div>
+    <br />
+  </main>
   <footer>Footer</footer>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const colors = [
+  'dark',
+  'light',
+  'primary',
+  'secondary',
+  'accent',
+  'success',
+  'info',
+  'warning',
+  'danger',
+];
+
+const container = ref(null);
+
+onMounted(() => {
+  if (container.value) {
+    const dom = container.value as any;
+
+    const create = (element: string, prop: string, color: string) => {
+      const el = document.createElement(element);
+      el.classList.add(`${prop}-${color}`);
+      el.innerText = `${prop}-${color}`;
+      el.style.marginBottom = '10px';
+      dom.appendChild(el);
+    };
+
+    const hr = (color: string) => {
+      const el = document.createElement('hr');
+      el.classList.add(color);
+      el.style.marginBottom = '10px';
+      dom.appendChild(el);
+    };
+
+    colors.forEach((color) => create('div', 'bg', color));
+    colors.forEach((color) => create('div', 'text', color));
+    colors.forEach((color) => create('div', 'border', color));
+    colors.forEach((color) => hr(color));
+  }
+});
+</script>

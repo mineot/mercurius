d7949b69ce5e8c5557bac93c2b2a721259c64042
mineot <mineot@gmail.com>
2025-03-27 23:26:52 -0300

    master - add bootstrap icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name') }}</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         @vite(['resources/core/app.css', 'resources/core/app.ts'])
     </head>
     <body data-bs-theme="dark">
         <h1>Bem-vindo ao Mercurius</h1>
         <p>Este é o seu novo construtor de websites!</p>
+        <div>
+            <i class="bi bi-house"></i>
+        </div>
     </body>
 </html>

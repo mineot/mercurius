8b60e44a9ec8b9ee19e2c00d429c799bcb285d9b
mineot <mineot@gmail.com>
2025-04-02 23:50:30 -0300

    master - sidebars

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,10 +23,31 @@
         </div>
     </nav>
 </header>
+
 <div class="space-x margin-x" >Profissional</div>
 <div class="space-x margin-x">Especialidades</div>
 <div class="space-x margin-x">Portifólio</div>
+
 <footer class="footer space margin-x">Footer</footer>
-<aside class="sidebar">Aside</aside>
+
+<aside class="sidebar">
+    <div class="sidebar-brand">
+        <img src="{{ $profile->avatar }}" alt="{{ $profile->name }}">
+        <span>{{ $profile->name }}</span>
+        <span>{{ $profile->summary }}</span>
+    </div>
+    <hr>
+    <nav class="sidebar-menu">
+        <ul class="sidebar-links">
+            <li>
+                <a class="navbar-link" href="#">
+                    <i class="bit bi-link"></i>
+                    <span>Link</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
+
 <div class="backdrop-glass" onclick="window.sidebar()"></div>
 @endsection

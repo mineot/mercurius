cd874cdb5a9180e214bbed374822bc73e04d6e14
mineot <mineot@gmail.com>
2025-04-03 23:45:36 -0300

    master - sidebar component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,24 @@
+<aside class="sidebar">
+
+    <div class="sidebar-brand">
+        <img class="sidebar-avatar" src="{{ $profile->avatar }}" alt="{{ $profile->name }}">
+        <span class="sidebar-name">{{ $profile->name }}</span>
+        <span class="sidebar-summary">{{ $profile->summary }}</span>
+    </div>
+
+    <hr class="sidebar-divider">
+
+    <nav class="sidebar-menu">
+
+        <ul class="sidebar-links">
+            <li>
+                <a class="sidebar-link" href="#">
+                    <i class="bit bi-link"></i>
+                    <span>Link</span>
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+
+</aside>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,31 +2,10 @@
 
 @section("content")
 <x-header :profile="$profile" />
-
 <div class="space-x margin-x" >Profissional</div>
 <div class="space-x margin-x">Especialidades</div>
 <div class="space-x margin-x">Portifólio</div>
-
 <footer class="footer space margin-x">Footer</footer>
-
-<aside class="sidebar">
-    <div class="sidebar-brand">
-        <img src="{{ $profile->avatar }}" alt="{{ $profile->name }}">
-        <span>{{ $profile->name }}</span>
-        <span>{{ $profile->summary }}</span>
-    </div>
-    <hr>
-    <nav class="sidebar-menu">
-        <ul class="sidebar-links">
-            <li>
-                <a class="navbar-link" href="#">
-                    <i class="bit bi-link"></i>
-                    <span>Link</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</aside>
-
+<x-sidebar :profile="$profile" />
 <div class="backdrop-glass" onclick="window.sidebar()"></div>
 @endsection

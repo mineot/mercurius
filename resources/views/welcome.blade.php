8d4f6d1a31cad3fb6c35cf17d3bf2919e3156391
mineot <mineot@gmail.com>
2025-04-03 23:19:25 -0300

    master - header component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,7 @@
 @extends("layouts/guest")
 
 @section("content")
-<header class="navbar space margin-x">
-    <div class="navbar-brand">
-        <img class="navbar-avatar" src="{{ $profile->avatar }}" alt="{{ $profile->name }}">
-        <div class="navbar-title">
-            <span class="navbar-name">{{ $profile->name }}</span>
-            <span class="navbar-summary">{{ $profile->summary }}</span>
-        </div>
-    </div>
-    <nav class="navbar-menu">
-        <ul class="menu-links">
-            <li>
-                <a class="navbar-link" href="#">
-                    <i class="bit bi-link"></i>
-                    <span>Link</span>
-                </a>
-            </li>
-        </ul>
-        <div class="menu-toggle" onclick="window.sidebar()">
-            <i class="bi bi-list"></i>
-        </div>
-    </nav>
-</header>
+<x-header :profile="$profile" />
 
 <div class="space-x margin-x" >Profissional</div>
 <div class="space-x margin-x">Especialidades</div>

6efddbac923c64ce6537fd41312293712c96a479
mineot <mineot@gmail.com>
2025-04-06 10:22:33 -0300

    clean project'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
-@extends("layouts/guest")
+@extends("main")
 
 @section("content")
-<x-header :profile="$profile" toogleClick="window.sidebar()" />
+<h1>Welcome</h1>
+{{-- <x-header :profile="$profile" toogleClick="window.sidebar()" />
 <div class="app-space-x">Profissional</div>
 <div class="app-space-x">Especialidades</div>
 <div class="app-space-x">Portifólio</div>
 <footer class="footer app-space">Footer</footer>
 <x-sidebar :profile="$profile" />
-<x-backdrop-glass click="window.sidebar()" />
+<x-backdrop-glass click="window.sidebar()" /> --}}
 @endsection

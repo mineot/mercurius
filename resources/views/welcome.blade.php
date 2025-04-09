ef332984ac44b81de48cb2b0932874f3310af44d
mineot <mineot@gmail.com>
2025-04-08 21:54:15 -0300

    improvement layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,12 @@
 
 @section("content")
 <x-layout>
-
     <x-navbar toogleClick="window.sidebar()">
         <x-slot name="brand">Profile</x-slot>
     </x-navbar>
-
     <x-sidebar />
-
     <x-main />
-
     <x-footer />
-
     <x-glass click="window.sidebar()" />
-
 </x-layout>
 @endsection

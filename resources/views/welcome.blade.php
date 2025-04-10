7c8a567099db6b8faaa5766736ed013abb27f40f
mineot <mineot@gmail.com>
2025-04-09 23:25:32 -0300

    create sidebar profile component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,11 @@
         <x-profile-header :profile="$profile" />
     </x-slot:brand>
 
+    <x-slot:sidebarBrand>
+        <x-profile-sidebar :profile="$profile" />
+    </x-slot:sidebarBrand>
+
     <x-slot:nav>Navigation</x-slot:nav>
-    <x-slot:sidebarBrand>Sidebar Brand</x-slot:sidebarBrand>
     <x-slot:sidebarContent>Sidebar Content</x-slot:sidebarContent>
     <x-slot:footer>Footer</x-slot:footer>
     <h1>Welcome</h1>

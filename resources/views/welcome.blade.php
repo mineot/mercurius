ccb34b51b0563ab1921e03296a42453296f7281b
mineot <mineot@gmail.com>
2025-04-10 22:45:57 -0300

    nav links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,18 @@
     </x-slot:sidebarBrand>
 
     <x-slot:nav>
-        Navigation
+        <a href="#" class="nav-link">
+            <i class="bi bi-house-fill"></i>
+            <span>Home</span>
+        </a>
+        <a href="#" class="nav-link">
+            <i class="bi bi-person-fill"></i>
+            <span>Sobre Mim</span>
+        </a>
+        <a href="#" class="nav-link">
+            <i class="bi bi-book"></i>
+            <span>Artigos</span>
+        </a>
     </x-slot:nav>
 
     <x-slot:sidebarContent></x-slot:sidebarContent>

4cb1685dc74eff2717827d38fcb1f65ba2c06b61
mineot <mineot@gmail.com>
2025-04-10 23:30:33 -0300

    adjust navbar links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,30 @@
 
     <x-slot:nav>
         <a href="#" class="nav-link">
-            <i class="bi bi-house-fill"></i>
-            <span>Home</span>
+            <i class="bi bi-house-fill icon"></i>
+            <span class="label">Home</span>
         </a>
         <a href="#" class="nav-link">
-            <i class="bi bi-person-fill"></i>
-            <span>About Me</span>
+            <i class="bi bi-person-fill icon"></i>
+            <span class="label">About Me</span>
         </a>
         <a href="#" class="nav-link">
-            <i class="bi bi-book"></i>
-            <span>Articles</span>
+            <i class="bi bi-book icon"></i>
+            <span class="label">Articles</span>
         </a>
     </x-slot:nav>
+
+    <x-slot:sidebarSocialMedia>
+        Social Medias
+    </x-slot:sidebarSocialMedia>
+
+    <x-slot:sidebarNav>
+        Navigation
+    </x-slot:sidebarNav>
+
+    <x-slot:sidebarContent>
+        Content
+    </x-slot:sidebarContent>
 
     <x-slot:footer>Footer</x-slot:footer>
 

3eff26d5146a631888348cb31e6d16748133de57
mineot <mineot@gmail.com>
2025-04-15 20:02:19 -0300

    form layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,21 +4,12 @@
 <div class="layout-center">
     <div class="layout-center-content">
 
-        <h1>Layout Center</h1>
-        {{-- <form class="form-layout">
-            <h1 class="form-title">Login</h1>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input id="email" type="email" />
-                <div class="message">Message</div>
+        <div class="flex flex-col gap-2">
+            <input type="text" class="input" />
+            <div>
+                <button class="btn">Button</button>
             </div>
-            <div class="form-group">
-                <label for="email">Password</label>
-                <input id="password" type="password" />
-                <div class="message">Message</div>
-            </div>
-            <button type="button" class="form-action">Button</button>
-        </form> --}}
+        </div>
 
     </div>
 </div>

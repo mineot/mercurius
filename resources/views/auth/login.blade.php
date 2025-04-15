d551ead4ef7706fbef9685794664fcc7fa04a955
mineot <mineot@gmail.com>
2025-04-14 21:15:30 -0300

    some improvements

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,34 +9,16 @@
             <div class="form-group">
                 <label for="email">Email</label>
                 <input id="email" type="email" />
-                <div>Message</div>
+                <div class="message">Message</div>
             </div>
             <div class="form-group">
                 <label for="email">Password</label>
                 <input id="password" type="password" />
-                <div>Message</div>
+                <div class="message">Message</div>
             </div>
             <button type="button" class="form-action">Button</button>
         </form>
 
     </div>
 </div>
-
-{{-- <div class="auth-index">
-    <h1 class="title text-center">Login</h1>
-
-    <form action="">
-        <div class="formGroup">
-            <label for="email">E-mail</label>
-            <input type="email" id="email" />
-        </div>
-
-        <div class="formGroup">
-            <label for="password">Password</label>
-            <input type="password" id="password" />
-        </div>
-
-        <button class="mt-4" type="button">Login</button>
-    </form>
-</div> --}}
 @endsection

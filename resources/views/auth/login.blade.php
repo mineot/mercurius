ec9531165727afa74b8961ade26b4d3f58c45683
mineot <mineot@gmail.com>
2025-04-18 17:39:30 -0300

    i18n - create terms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends("core/main")
 
 @section("content")
-<x-layout-center title="Login">
+<x-layout-center :title="__('terms.login')">
     <form class="flex flex-col gap-2">
-        <x-form-control id="email" name="email" type="email" label="Email" />
-        <x-form-control id="password" name="password" type="password" label="Password" />
-        <button class="btn">Login</button>
+        <x-form-control id="email" name="email" type="email" :label="__('terms.email')" />
+        <x-form-control id="password" name="password" type="password" :label="__('terms.password')" />
+        <button class="btn">{{ __('terms.login') }}</button>
     </form>
 </x-layout-center>
 @endsection

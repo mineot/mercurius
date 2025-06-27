593e63dd5ebbfc4550950ff80cd8080159f83206
mineot <mineot@gmail.com>
2025-06-26 21:45:24 -0300

    react - create a theme css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import "./index.css";
-
+import "./styles/main.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./components/App.tsx";
 
 createRoot(document.getElementById("root")!).render(

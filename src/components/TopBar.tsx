12c11fb9e0da1befb1f60f3626a2f692e71c5aec
mineot <mineot@gmail.com>
2025-06-30 22:19:58 -0300

    react - create a typography

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,3 @@
+export function TopBar() {
+  return <div>Topbar</div>;
+}
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,3 +1,18 @@
 export function Welcome() {
-  return <div className="title">Welcome to Mercurius</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="text-3xl font-bold">Typography</div>
+      <hr />
+      <div>
+        <h1>Welcome to Mercurius</h1>
+        <h2>Welcome to Mercurius</h2>
+        <h3>Welcome to Mercurius</h3>
+        <h4>Welcome to Mercurius</h4>
+        <h5>Welcome to Mercurius</h5>
+        <h6>Welcome to Mercurius</h6>
+        <div>This is a regular text!</div>
+        <div className="emphasis-text">This is an emphasis text!</div>
+      </div>
+    </div>
+  );
 }

646aed241a8f06eb04563c54f9f419805d927ce3
mineot <mineot@gmail.com>
2025-08-04 22:43:34 -0300

    :coffin: clear: clear initial codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import "./index.css";
+import "./main.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.tsx";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div>Welcome to Mercurius</div>
   </StrictMode>
 );

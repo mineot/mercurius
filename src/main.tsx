7f5b4b3cc6eb845ec74e20168f41b06d104312e5
mineot <mineot@gmail.com>
2025-08-16 21:38:56 -0300

    :sparkles: feat: add react routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import './main.css';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 
-import { Layout } from '@/components/Layout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
+import About from '@/pages/about';
+import NotFound from '@/pages/notfound';
+import Welcome from '@/pages/welcome';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout></Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );

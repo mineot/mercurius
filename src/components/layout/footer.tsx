b0e651c9e27d16cc75d114ad41732940f1e1ab65
mineot <mineot@gmail.com>
2025-08-31 17:30:56 -0300

    :sparkles: feat: create footer component

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-row flex-nowrap gap-5">
+      <div>
+        <div>Imagem, Nome do Projeto</div>
+        <div>Descrição geral e curta</div>
+        <div>Endereço</div>
+        <div>Telefone e Email</div>
+        <div>Link Sobre Mim</div>
+        <div>Link Contato</div>
+        <div>Copyright</div>
+      </div>
+      <div>Quick Links (idem nav)</div>
+      <div>Social Media</div>
+      <div>Legal Infos</div>
+    </footer>
+  );
+}
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from './header';
+import { Footer } from './footer';
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -10,7 +11,7 @@ export function Layout(props: LayoutProps) {
       <Header />
       <main className="l-main">
         <section>{props.children}</section>
-        <footer>Footer</footer>
+        <Footer />
       </main>
     </>
   );
